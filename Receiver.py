a70339bf573d6b84ea91673142718309d43cdaed
JoseReyesRIT <43761969+JoseReyesRIT@users.noreply.github.com>
2019-10-27 15:01:58 -0400

    Deleted comments

--- Receiver.py
+++ Receiver.py
@@ -2,7 +2,6 @@
 import sched
 import time
 import datetime
-import Monitor as mon
 import os
 
 
@@ -16,14 +15,10 @@ class Receiver:
     isAlive = False
 
     def __init__(self, q, q2):
-        # print('init receiver')
         self.queue = q
         self.queue2 = q2
         self.s = sched.scheduler(time.time, time.sleep)
         self.pit_a_pat()
-
-    # def __init__(self):
-    # self.lastUpdatedTime = datetime
 
     def updateTime(self):
         print(f"{os.getpid()}: - Beat Received")
@@ -31,7 +26,6 @@ class Receiver:
         print(f"{os.getpid()}: - Time of last beat: {self.lastUpdatedTime}")
         self.expireTime = self.lastUpdatedTime + datetime.timedelta(seconds=5)
         print(f"{os.getpid()}: - New expire time: {self.expireTime}")
-        # print(self.expireTime - self.lastUpdatedTime)
 
     def pit_a_pat(self):
         if not self.queue.empty():
@@ -41,7 +35,6 @@ class Receiver:
             self.queue2.put(True)
         else:
             self.queue2.put(False)
-        # mon.Log(self.isAlive)
         self.s.enter(10, 1, self.pit_a_pat)
         self.s.run()
 
@@ -52,13 +45,5 @@ class Receiver:
                 print(f"{os.getpid()}: - No beat received after expire time")
                 return False
         else:
-            # print(q.get());
             print(f"{os.getpid()}: - Sender is still beating")
             return True
-
-# if __name__ == "__main__":
-#     test = Receiver()
-#     p2 = multiprocessing.Process(target=test.start())
-#     p2.start()
-#
-#
--- Sender.py
+++ Sender.py
@@ -14,15 +14,10 @@ class Sender:
     def __init__(self, q):
         self.queue = q
         self.s = sched.scheduler(time.time, time.sleep)
-        # print('init sender')
         self.start()
 
     def locate_mycar(self):
         car_location = geocoder.ip('me')
-        # print("LATITUDE AND LONGTITUDE: ", car_location.latlng)
-        # print("CITY: ", car_location.city)
-        # print("STATE: ", car_location.state)
-        # print("COUNTRY: ", car_location.country)
         return f"{os.getpid()}: -  Car coordinates: {car_location.latlng}. City: {car_location.city}. State: {car_location.state}." \
                f" Country: {car_location.country}"
 
@@ -43,7 +38,6 @@ class Sender:
         self.s.run()
 
     def randomFault(self):
-        # print(self.maxRandom)
         rand1 = random.randint(1, self.maxRandom)
         print(f"{os.getpid()}: - RANDOM 1: {rand1}")
         rand2 = random.randint(1, self.maxRandom)
